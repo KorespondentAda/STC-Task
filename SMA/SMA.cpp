7cd859c9db9745f854ea3f1149c3263c4588afc8
KorespondentAda <0kononov0@gmail.com>
2023-10-09 21:59:22 +0300

    Initial commit

---
+++ SMA/SMA.cpp
@@ -0,0 +1,10 @@
+#include <vector>
+
+SAMPLE_T SimpleMovingAverageNext(int k, SAMPLE_T previous) {
+	return previous;
+}
+
+std::vector<SAMPLE_T> SimpleMovingAverage(int k, std::vector<SAMPLE_T> data) {
+	return data;
+}
+
---
+++ main.cpp
@@ -0,0 +1,25 @@
+#include <iostream>
+#include <vector>
+#include "SMA.hpp"
+
+int main() {
+	/// Read number of data points #n and window size #k, in samples
+	int n, k;
+	std::cin >> n >> k;
+	std::vector<SAMPLE_T> data(n);
+
+	/// Then read #n samples from stdin
+	for (int i = 0; i < n; i++) {
+		std::cin >> data[i];
+	}
+
+	std::vector<SAMPLE_T> sma = SimpleMovingAverage(k, data);
+
+	/// Write calculated SMAs to stdout
+	for (int i = 0; i < sma.size(); i++) {
+		std::cout << sma[i] << std::endl;
+	}
+
+	return 0;
+}
+
