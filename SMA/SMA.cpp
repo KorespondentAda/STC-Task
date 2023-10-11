25e79b000ef01244f2eca7d458e6502fd1085fce
KorespondentAda <0kononov0@gmail.com>
2023-10-11 14:31:07 +0300

    SMA func works
    Simple realization of algorithm.
    Without `...Next()` it better than with it because of many parameters and one-line realization.

--- SMA/SMA.cpp
+++ SMA/SMA.cpp
@@ -1,10 +1,23 @@
 #include <vector>
 
-SAMPLE_T SimpleMovingAverageNext(int k, SAMPLE_T previous) {
-	return previous;
-}
-
 std::vector<SAMPLE_T> SimpleMovingAverage(int k, std::vector<SAMPLE_T> data) {
-	return data;
+	int smaSize = data.size() - (k - 1);
+	std::vector<SAMPLE_T> sma(smaSize);
+
+	/// First SMA value is just average of #data first #k elements
+	sma[0] = 0;
+	for (int i = 0; i < k; i++) {
+		sma[0] += data[i];
+	}
+	sma[0] /= k;
+
+	/// Every next value calculated iteratively
+	for (int i = 0; i < smaSize-1; i++) {
+		int idxPrev = i;
+		int idxNext = i + k;
+		sma[i + 1] = sma[i] + ((data[idxNext] - data[idxPrev]) / k);
+	}
+
+	return sma;
 }
 
--- SMA/SMA.hpp
+++ SMA/SMA.hpp
@@ -2,12 +2,10 @@
 
 /// @def SAMPLE_T Type of data. Defined by CMake. Can be `float` or `double`
 
-/// Calculate next SMA value by previous
-/// Can be used directly for continuous processing
-/// @return New SMA value
-SAMPLE_T SimpleMovingAverageNext(int k, SAMPLE_T previous);
-
 /// Calculate SMA value of #data points with #k point size window
-/// @return Output data vector
+///
+/// @param k Window size, samples
+/// @param data Input data samples
+/// @return Vector of calculated SMAs
 std::vector<SAMPLE_T> SimpleMovingAverage(int k, std::vector<SAMPLE_T> data);
 
