40d494394ce6fde8a7e77100ff59f13bb99e0090
KorespondentAda <0kononov0@gmail.com>
2023-10-11 18:36:03 +0300

    Input error checks

--- main.cpp
+++ main.cpp
@@ -9,6 +9,29 @@ int main() {
 	/// Read number of data points #n and window size #k, in samples
 	int n, k;
 	std::cin >> n >> k;
+
+	if (std::cin.eof()) {
+		std::cerr << "Wrong input! Must be `<n> <k> [<s>...]`" << std::endl;
+		std::cerr << "Here <n> is sample count, <k> is window size, <s> is actual samples" << std::endl;
+		return 1;
+	}
+
+	if (n < k) {
+		std::cerr << "Wrong input! Window size can't be larger than data!" << std::endl;
+		return 1;
+	}
+
+	if (n <= 0) {
+		std::cerr << "Wrong input! Sample count can't be less than 1!" << std::endl;
+		return 1;
+	}
+
+	if (k <= 0) {
+		std::cerr << "Wrong input! Window size can't be less than 1!" << std::endl;
+		return 1;
+	}
+
+
 	std::vector<SAMPLE_T> data(n, 0);
 
 	/// Then read #n samples from stdin
